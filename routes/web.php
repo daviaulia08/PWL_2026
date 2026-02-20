03fc4967687425e423e69209967bc3e1bef65e35
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 02:28:21 +0700

    Route Parameters : route username

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/about', function () {
-    return 'Nim 244107020093, Nama saya Davi Aulia';
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
 });
 
