17051ca243edc09766c00b67ed1c612e1345235f
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 03:45:24 +0700

    Route Parameters : route articles

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route; 
 
-Route::get('/posts/{post}/comments/{comment}', 
-function ($postId, $commentId) {
-    return 'Pos ke-' .$postId. "Komentar ke-: " . $commentId;
+Route::get('/articles/{id}', function ($articleId) {
+    return 'Artikel ke-' .$articleId;
 });
 
