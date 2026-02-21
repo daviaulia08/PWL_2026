82832a746d1c88ea149ed132077b82dcbdd1596a
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 19:46:48 +0700

    Menampilkan View dari Controller

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,5 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]); 
 
-Route::get('/greeting', function () {
-    return view('blog.hello', ['name' => 'Davi']);
-});
+Route::get('/greeting', [WelcomeController::class, 
+'greeting']);
