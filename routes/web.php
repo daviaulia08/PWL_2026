f1a44c6c7113a36541e9526772fe617c7ed119b2
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 17:22:48 +0700

    Redirect Routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,4 @@
 
 use Illuminate\Support\Facades\Route; 
 
-
-Route::prefix('admin')->group (function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);        
-    Route::get('/event', [EventController::class, 'index']);
-});
+Route::redirect('/here', '/there');
