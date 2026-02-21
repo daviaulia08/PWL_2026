5b6ba2288b2a84a4655f66d4c0ff5b64b877ed67
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 19:27:11 +0700

    View : file hello.blade.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]); 
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Davi']);
+});
