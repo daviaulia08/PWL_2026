ba4aed6509aecba3b7d3eeb6d38dd76c2c145df7
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 17:11:21 +0700

    Route Name

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Support\Facades\Route; 
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya ' .$name;
-});
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+// Generating URLs..
+$url = route('profile');
+
+// Generating Redirects...
+return redirect()->route('profile');
+
+
+
 
