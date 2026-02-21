b500655a7636b4c932fa4ba2357a8d1b269c9ecf
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 17:20:09 +0700

    Redirect Routes

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Support\Facades\Route; 
 
-Route::middleware(['first', 'second'])->group(function () {
-    Route::get('/', function () {
-        // Uses first & second Middleware
-    });
-
-Route::get('/user/profile', function () {
-        // Uses first & second Middleware
-    });
-});
-
-Route::domain('{account}.example.com')->group (function () {
-    Route::get('user/{id}', function ($account, $id) {
-        //
-    });
-});
-
-Route::middleware('auth')->group (function () {
+Route::prefix('admin')->group (function () {
     Route::get('/user', [UserController::class, 'index']);
     Route::get('/post', [PostController::class, 'index']);        
     Route::get('/event', [EventController::class, 'index']);
