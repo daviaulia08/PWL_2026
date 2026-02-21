249e899ff1f15c57222507694bb8d97a8242ca4c
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 17:56:39 +0700

    Modifikasi Controller seperti hasil Routing (Praktikum Sebelumnya)

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\PageController;
 
-Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'hello']);
