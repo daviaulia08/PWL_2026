d754289a9f61cb5feacf8cbbeebb94e2ea27c0ab
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 19:10:09 +0700

    Modifikasi Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles/{id}', [PageController::class, 'hello']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
