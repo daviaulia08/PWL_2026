75f74e608f2110dca7fcc053451a7b155bb769e7
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-21 19:19:14 +0700

    Resource Controller : PhotoController

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]); 
